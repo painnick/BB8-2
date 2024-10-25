fd792b5633caf11053cf7980e299b0f95bca7ce8
painnick <painnick@gmail.com>
2024-10-26 01:51:41 +0900

    UPDATE. python

--- python/main.py
+++ python/main.py
@@ -9,6 +9,7 @@ import urllib.request
 from urllib.error import HTTPError, URLError
 import numpy as np
 import logging
+import time
 
 logger = logging.getLogger("main")
 logger.setLevel(logging.DEBUG)
@@ -16,6 +17,9 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
+
+cv2.namedWindow('mp', cv2.WINDOW_NORMAL)
+# cv2.setWindowProperty('mp', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
@@ -27,25 +31,30 @@ face_lost = time.time()
 direction = 'none'
 
 # For webcam input:
-with mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.6) as face_detection:
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.7) as face_detection:
   while True:
     try:
       img_resp = urllib.request.urlopen(url=host + '/capture', timeout=1)
     except (HTTPError, URLError) as error:
       logging.error('Data not retrieved because %s', error)
+      if cv2.waitKey(1000) == 27:
+        break
       continue
     except ConnectionResetError:
       logger.error('Connection Reset!')
+      if cv2.waitKey(1000) == 27:
+        break
       continue
 
     try:
-      imgnp = np.array(bytearray(img_resp.read()),dtype=np.uint8)
+      imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
     except socket.timeout:
       logger.error('Timeout!')
+      if cv2.waitKey(1000) == 27:
+        break
       continue
 
-    image = cv2.imdecode(imgnp,-1)
+    image = cv2.imdecode(imgnp, -1)
 
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
@@ -57,33 +66,39 @@ with mp_face_detection.FaceDetection(
     # Draw the face detection annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.detections: # Found
+    if results.detections:  # Found
       face_found = time.time()
       for detection in results.detections:
         box = detection.location_data.relative_bounding_box
-        center = box.xmin + (box.width / 2)
+        center = box.xmin + (box.width / 2)  # Calculate only the space represented within the screen
         mp_drawing.draw_detection(image, detection)
-        if 0.4 > center or center > 0.6:
+        if 0.3 > center or center > 0.7:
           if center > 0.5:
             direction = 'right'
-            urllib.request.urlopen(host + '/motor?dir=%s&found=true' %(direction))
-            # logger.debug(direction)
+            logger.debug(direction)
+            urllib.request.urlopen(host + '/motor?dir=%s&found=true' % (direction))
+            time.sleep(0.3)
           else:
             direction = 'left'
-            urllib.request.urlopen(host + '/motor?dir=%s&found=true' %(direction))
-            # logger.debug(direction)
-        #else:
-        #    direction = 'none'
-        #    urllib.request.urlopen(host + '/motor?dir=%s&found=true' % (direction))
-      face_lost = time.time()
-    else: # Lost
+            logger.debug(direction)
+            urllib.request.urlopen(host + '/motor?dir=%s&found=true' % (direction))
+            time.sleep(0.3)
+        else:
+          logger.debug('Found(Center)')
+          urllib.request.urlopen(host + '/motor?dir=%s&found=true' % (direction))
+          cv2.imshow('mp', cv2.flip(image, 1))
+          cv2.waitKey(1000 * 5)
+      face_lost = time.time()  # Reset lost timing
+    else:  # Lost
       current_time = time.time()
       if current_time - face_lost > 2:
-        face_lost = time.time()
-        urllib.request.urlopen(host + '/motor?dir=%s&found=false' %(direction))
+        logger.debug('Lost')
         logger.debug(direction)
+        face_lost = time.time()
+        urllib.request.urlopen(host + '/motor?dir=%s&found=false' % (direction))
+        time.sleep(0.1)
 
     # Flip the image horizontally for a selfie-view display.
-    #cv2.imshow('MediaPipe Face Detection', cv2.flip(image, 1))
-    #if cv2.waitKey(5) & 0xFF == 27:
-    #  break
+    cv2.imshow('mp', cv2.flip(image, 1))
+    if cv2.waitKey(1) == 27:
+      break
